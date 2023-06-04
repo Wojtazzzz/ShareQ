c83d1ebc6ef0b6d664f178927760cf361c93b512
Wojtazzzz <marcin.witas72@gmail.com>
2023-06-04 19:54:54 +0200

    chore/prettier, eslint

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,11 +1,9 @@
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return (
-    <html lang="en">
-      <body>{children}</body>
-    </html>
-  );
+import type { ReactNode } from 'react';
+
+export default function RootLayout({ children }: { children: ReactNode }) {
+	return (
+		<html lang="pl">
+			<body>{children}</body>
+		</html>
+	);
 }
